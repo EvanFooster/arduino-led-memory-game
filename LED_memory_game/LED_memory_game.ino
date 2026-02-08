31383a3017b1ee0d3d916037603a3ba6624d04f5
Evan Foster <evanfoster2007@gmail.com>
2026-02-08 02:38:47 -0500

    Refactor: modularize game logic, improve input/reset handling

--- LED_memory_game/LED_memory_game.ino
+++ LED_memory_game/LED_memory_game.ino
@@ -5,34 +5,53 @@
 
 const int MIN_VAL = 1;
 const int MAX_VAL = 3;
-const int MAX_SEQUENCE = 5;
+const int MAX_SEQUENCE = 3;
 
 int sequence[MAX_SEQUENCE];
 
 int seqIndex = 0;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.setTimeout(10000000000);
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, LOW);
+  digitalWrite(ledPin3, LOW);
+  Serial.begin(9600);
   randomSeed(analogRead(A0));
 }
 
 
 void loop() {
-  //Add to sequence
+  addSeq();
+  flashSeq();
+
+  //Take user input and check if user failed
+  if(!takeInput()) {
+    lose();
+    return;
+  }
+  //Check if player has reached MAX_SEQUENCE
+  if(seqIndex == MAX_SEQUENCE) {
+    win();
+    return;
+  }
+  //Wait briefly to update sequence and flash again
+  delay(2000);
+}
+
+
+//add a random number to sequence
+void addSeq() {
   if (seqIndex < MAX_SEQUENCE) {
     sequence[seqIndex] = random(MIN_VAL, MAX_VAL + 1);
     seqIndex++;
-  //Print win message and reset if user completes length of MAX_SEQUENCE
-  } else {
-    Serial.print("Congratulations, you win!");
-    seqIndex = 0;
   }
+}
 
-  //Flash sequence to user
+//Flash sequence to user
+void flashSeq() {
   for(int i = 0; i < seqIndex; i++) {
     int led;
     if(sequence[i] == 1){
@@ -47,22 +66,34 @@ void loop() {
     digitalWrite(led, LOW);
     delay(500);
   }
-  
-  //Take user input
+}
+
+//Take user input
+bool takeInput() {
   for(int i = 0; i < seqIndex; i++){
     while(Serial.available() == 0){}
     int input = Serial.readStringUntil('\n').toInt();
-    //reset if user fails
+    //Return false if user fails
     if(input != sequence[i]) {
-      seqIndex = 0;
-      break;
+      return false;
     }
   }
-  //Wait briefly to update sequence and flash again
-  delay(2500);
+  return true;
 }
 
+//Print win message and reset if user completes length of MAX_SEQUENCE
+void win() {
+  Serial.println("Congratulations, you win!");
+  seqIndex = 0;
+  delay(2000);
+}
 
+//Print lose message and reset if user gives wrong sequence
+void lose() {
+  seqIndex = 0;
+  Serial.println("Incorrect!");
+  delay(2000);
+}
 
 
 
